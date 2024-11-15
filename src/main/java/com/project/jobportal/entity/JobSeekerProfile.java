26f32d86fd523adf727ef6979a9cc45b18feab73
PC1237 <simovanev@abv.bg>
2024-11-15 13:00:35 +0200

    JobSeekerProfile and RecruiterProfile

---
+++ src/main/java/com/project/jobportal/entity/JobSeekerProfile.java
@@ -0,0 +1,164 @@
+package com.project.jobportal.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
+public class JobSeekerProfile {
+    @Id
+    private int userAccountId;
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+    private String firstName;
+    private String lastNAme;
+    private String city;
+    private String state;
+    private String country;
+    private String workAuthorization;
+    private String employmentType;
+    private String resume;
+    @Column(nullable = true)
+    private String profilePhoto;
+    @OneToMany(targetEntity = Skills.class, cascade = CascadeType.ALL, mappedBy = "jobSeekerProfile")
+    private List<Skills> skills;
+
+    public JobSeekerProfile() {
+    }
+
+    public JobSeekerProfile(Users userId) {
+        this.userId = userId;
+    }
+
+    public JobSeekerProfile(int userAccountId, Users userId, String firstName, String lastNAme, String city, String state, String country, String workAuthorization, String employmentType, String resume, String profilePhoto, List<Skills> skills) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastNAme = lastNAme;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.workAuthorization = workAuthorization;
+        this.employmentType = employmentType;
+        this.resume = resume;
+        this.profilePhoto = profilePhoto;
+        this.skills = skills;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNAme() {
+        return lastNAme;
+    }
+
+    public void setLastNAme(String lastNAme) {
+        this.lastNAme = lastNAme;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getWorkAuthorization() {
+        return workAuthorization;
+    }
+
+    public void setWorkAuthorization(String workAuthorization) {
+        this.workAuthorization = workAuthorization;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public List<Skills> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skills> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerProfile{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastNAme='" + lastNAme + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", workAuthorization='" + workAuthorization + '\'' +
+                ", employmentType='" + employmentType + '\'' +
+                ", resume='" + resume + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                ", skills=" + skills +
+                '}';
+    }
+}
---
+++ src/main/java/com/project/jobportal/entity/Skills.java
@@ -0,0 +1,9 @@
+package com.project.jobportal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Skills {
+}
