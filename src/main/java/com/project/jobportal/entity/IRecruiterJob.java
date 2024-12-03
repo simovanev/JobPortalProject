96d1640830ecb91e5409fc8275345204a7d2303d
Simovanev <simovanev@abv.bg>
2024-12-03 04:26:19 +0200

    JobPostActivity display

---
+++ src/main/java/com/project/jobportal/entity/IRecruiterJob.java
@@ -0,0 +1,16 @@
+package com.project.jobportal.entity;
+
+public interface IRecruiterJob {
+    Long getTotalCandidates();
+    int getJob_post_id();
+    String getJob_title();
+    int getLocationId();
+    String getCity();
+    String getState();
+    String getCountry();
+    int getCompanyId();
+    String getName();
+
+
+
+}
--- src/main/java/com/project/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/project/jobportal/services/JobPostActivityService.java
@@ -1,8 +1,11 @@
 package com.project.jobportal.services;
 
-import com.project.jobportal.entity.JobPostActivity;
+import com.project.jobportal.entity.*;
 import com.project.jobportal.repository.JobPostActivityRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -13,5 +16,19 @@ public class JobPostActivityService {
     }
     public JobPostActivity addNew(JobPostActivity jobPostActivity){
         return postActivityRepository.save(jobPostActivity);
+    }
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        List<IRecruiterJob> recruiterJobsDtos = postActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+        for(IRecruiterJob recruiterJob : recruiterJobsDtos){
+            JobLocation loc=
+                    new JobLocation(recruiterJob.getLocationId(),recruiterJob.getCity()
+                            ,recruiterJob.getCountry(),recruiterJob.getState());
+            JobCompany com=new JobCompany(recruiterJob.getCompanyId(),recruiterJob.getName(),"");
+            recruiterJobsDtoList
+                    .add(new RecruiterJobsDto(recruiterJob.getTotalCandidates(),recruiterJob.getJob_post_id()
+                            ,recruiterJob.getJob_title(),loc,com));
+        }
+        return recruiterJobsDtoList;
     }
 }
