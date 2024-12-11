f604773e213d00cef6e4f029232a39e297adbd37
PC1237 <simovanev@abv.bg>
2024-12-11 12:41:15 +0200

    JobSeekerApply and JobSeekerSave

---
+++ src/main/java/com/project/jobportal/entity/JobSeekerApply.java
@@ -0,0 +1,85 @@
+package com.project.jobportal.entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"job", "userId"}))
+public class JobSeekerApply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_account_id")
+    private JobSeekerProfile userId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job", referencedColumnName = "jobPostId")
+    private JobPostActivity job;
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date applyDAte;
+    private String coverLetter;
+
+    public JobSeekerApply() {
+    }
+
+    public JobSeekerApply(Integer id, JobSeekerProfile userId, JobPostActivity job, Date applyDAte, String coverLetter) {
+        this.id = id;
+        this.userId = userId;
+        this.job = job;
+        this.applyDAte = applyDAte;
+        this.coverLetter = coverLetter;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    public Date getApplyDAte() {
+        return applyDAte;
+    }
+
+    public void setApplyDAte(Date applyDAte) {
+        this.applyDAte = applyDAte;
+    }
+
+    public String getCoverLetter() {
+        return coverLetter;
+    }
+
+    public void setCoverLetter(String coverLetter) {
+        this.coverLetter = coverLetter;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerApply{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", job=" + job +
+                ", applyDAte=" + applyDAte +
+                ", coverLetter='" + coverLetter + '\'' +
+                '}';
+    }
+}
